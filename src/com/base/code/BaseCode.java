e4e05589340520b2e8bf746e4934847349dd1f9d
jalal8068 <jalalahamad9@gmail.com>
2019-02-13 21:18:46 +0530

    base class code

---
+++ src/com/base/code/BaseCode.java
@@ -0,0 +1,10 @@
+package com.base.code;
+
+public class BaseCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Base Class Code");
+	}
+
+}
